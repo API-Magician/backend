26e8e198a2c4ebd9d7a08b6b5a58c4ff7a1a2cd6
mohamedsotohy <mo.hassan.9111@gmail.com>
2025-12-24 11:24:11 +0200

    feat: create user model and establish database connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 import { authenticated as customer_routes } from "./routes/authRoutes.js";
 import { general as genl_routes } from "./routes/generalRoutes.js";
 
 dotenv.config();
-
+connectDB();
 const app = express();
 app.use(express.json());
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log(`Server running on port 3000`));
--- src/routes/generalRoutes.js
+++ src/routes/generalRoutes.js
@@ -1,57 +1,62 @@
 import jwt from "jsonwebtoken";
 import express from "express";
+import User from "../models/User.js";
 const public_users = express.Router();
 
-const users = []; // fake db , replaced later with mongo
+const authenticatedUser = async (email, password) => {
+  // 1️⃣ Find user by email
+  const user = await User.findOne({ email });
 
-const authenticatedUser = (username, password) => {
-  let validusers = users.filter((user) => {
-    return user.username === username && user.password === password;
-  });
-  // Return true if any valid user is found, otherwise false
-  return validusers.length > 0;
+  // 2️⃣ If user does not exist
+  if (!user) {
+    return null;
+  }
+
+  // 3️⃣ Compare passwords (PLAIN TEXT — temporary)
+  if (user.password !== password) {
+    return null;
+  }
+
+  // 4️⃣ Auth successful
+  return user;
 };
 
-const doesExist = (username) => {
-  let userswithsamename = users.filter((user) => {
-    return user.username === username;
-  });
-  // Return true if any user with the same username is found, otherwise false
-  return userswithsamename.length > 0;
-};
-
-public_users.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+public_users.post("/register", async (req, res) => {
+  const { username, password, email } = req.body;
 
   // Check if both username and password are provided
-  if (!username || !password)
-    return res.status(404).send("Unable to register user.");
+  if (!username || !password || !email)
+    return res.status(400).send("Unable to register user."); //bad request
 
-  if (doesExist(username)) return res.status(404).send("User already exists!");
+  // check dublecate users
+  if (
+    (await User.findOne({ name: username })) ||
+    (await User.findOne({ email: email }))
+  )
+    return res.status(409).send("User already exists!"); //409 Conflict
 
-  // Add the new user to the users array
-  users.push({ username: username, password: password });
+  // Add the new user
+  const user = new User({ name: username, password: password, email: email });
+  await user.save();
   return res
     .status(200)
     .send("User successfully registered. Now you can login");
 });
 
 public_users.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { email, password } = req.body;
 
   // Check if username or password is missing
-  if (!username || !password) {
-    return res.status(404).json({ message: "Error logging in" });
+  if (!email || !password) {
+    return res.status(400).json({ message: "Error logging in" });
   }
 
-  // Authenticate user
-  if (authenticatedUser(username, password)) {
+  if (authenticatedUser(email, password)) {
+    // Authenticate user
     // Generate JWT access token
     let accessToken = jwt.sign(
       {
-        username: username,
+        username: username, //try later with user id
       },
       process.env.JWT_SECRET,
       { expiresIn: 30 } // 30 second just for testing
@@ -67,5 +72,4 @@ public_users.post("/login", (req, res) => {
       .json({ message: "Invalid Login. Check username and password" });
   }
 });
-
 export const general = public_users;
